541d862c3f68dda57ac1a176c9568b6f7eec2ca6
Pedda Narasappa Gari Naresh <devnareshtx@gmail.com>
2025-11-18 05:36:05 +0000

    in this i need to shop first then new (_for element <SubNav>_)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,12 +8,12 @@ export const PRIMARY_NAV_LINKS = [
 ];
 
 export const SECONDARY_NAV_LINKS = [
+  { name: 'Shop', href: '/shop' },
   { name: 'New', href: '/shop?category=new' },
   { name: 'Braids', href: '/shop?category=braids' },
   { name: 'Weaves', href: '/shop?category=weaves' },
   { name: 'Crochet', href: '/shop?category=crochet' },
   { name: 'Ponytails', href: '/shop?category=ponytails' },
-  { name: 'Shop', href: '/shop' },
 ];
 
 export const NAV_LINKS = [...PRIMARY_NAV_LINKS, ...SECONDARY_NAV_LINKS];
