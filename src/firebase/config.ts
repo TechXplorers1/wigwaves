73ab493b1371a1fbdbf94d85bd7d9a654166b805
Prakash SK <prakashsk.tx@gmail.com>
2025-11-19 20:07:30 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,8 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyB1c1Nyl2eWxWR1ji4f8xdDVN8XxaGGego",
   authDomain: "wigwaves-3b7aa.firebaseapp.com",
   projectId: "wigwaves-3b7aa",
-  storageBucket: "wigwaves-3b7aa.appspot.com",
+  storageBucket: "wigwaves-3b7aa.firebasestorage.app",
   messagingSenderId: "752867350514",
-  appId: "1:752867350514:web:5be36447c11c141cc8bd6b",
-  measurementId: "G-SFY5MFV662"
+  appId: "1:752867350514:web:fa6554e6d582a809c8bd6b"
 };
