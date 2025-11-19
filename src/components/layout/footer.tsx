2cc1690c4c930f1b34e7fc50a3ca043dbb8c5844
Prakash SK <prakashsk.tx@gmail.com>
2025-11-19 08:55:58 +0000

    make it center (_for element <Footer>_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,28 +10,28 @@ export default function Footer() {
     <footer className="bg-card border-t">
       <div className="container py-12">
         <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col items-center text-center gap-2">
             <MapPin className='w-7 h-7 text-primary mb-2'/>
             <h3 className="font-semibold text-lg">Our Office Address</h3>
             <address className="not-italic text-muted-foreground">
                 Eleganza House, 15B Joseph Street, Marina, Lagos, Nigeria
             </address>
           </div>
-           <div className="flex flex-col gap-2">
+           <div className="flex flex-col items-center text-center gap-2">
             <Mail className='w-7 h-7 text-primary mb-2'/>
             <h3 className="font-semibold text-lg">General Enquiries</h3>
             <p className="text-muted-foreground">
                 <Link href="mailto:ng-contact@wigwaves.com" className="hover:text-primary">ng-contact@wigwaves.com</Link>
             </p>
           </div>
-           <div className="flex flex-col gap-2">
+           <div className="flex flex-col items-center text-center gap-2">
             <Phone className='w-7 h-7 text-primary mb-2'/>
             <h3 className="font-semibold text-lg">Call Us</h3>
              <p className="text-muted-foreground">
                 <Link href="tel:+2349098101010" className="hover:text-primary">+234 909 810 1010</Link>
             </p>
           </div>
-           <div className="flex flex-col gap-2">
+           <div className="flex flex-col items-center text-center gap-2">
              <h3 className="font-semibold text-lg">Follow Us</h3>
             <div className="flex gap-4 mt-2">
                 <Link href="#" className="text-muted-foreground hover:text-primary">
